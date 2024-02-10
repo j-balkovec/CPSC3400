fca2f5bb198f4ea5009412be539f01517effe1d2
Jakob <jbalkovec@seattleu.edu>
2024-02-10 13:59:20 -0800

    added desc

---
+++ HW5/hw5.fsx
@@ -0,0 +1,25 @@
+//{
+//  @course: CPSC 3400
+//  @author: Jakob Balkovec
+//  @instructor: B. Diaz Acosta
+//
+//  @file: hw2.py
+//  @due: Mon, Feb 12th [22:00]
+//
+//  @submission_dir: /home/fac/bdiazacosta/submit/cpsc3400/hw4_submit
+//
+//  @brief
+//
+//  This file contains three F# exercises for homework 4:
+//  1. replace list origVal newVal
+//    Replace each occurrence of origVal in list with newVal.
+//  2. mergeList listA listB
+//    Merge two lists alternately.
+//  3. Dictionary functions:
+//    - search: Returns the value corresponding to a given key.
+//    - insert: Inserts a key-value pair into the dictionary if the key is not already present.
+//    - remove: Removes a key-value pair from the dictionary if the key is present.
+//    - count: Counts the number of elements satisfying a given condition.
+//    - twoDigitCount: Counts the number of entries with a two-digit positive integer value.
+//}
+
